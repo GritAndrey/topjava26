81a830f511d6b2ee9b317c643100cd27ace70e74
Gritsunick Andrey <stendarr@gmail.com>
2022-07-23 15:20:36 +0300

    Added getWithMeals to AdminRestController, ProfileRestController, AbstractUserController Used : JsonManagedReference, JsonBackReference annotations in model.

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -48,4 +48,9 @@ public abstract class AbstractUserController {
         log.info("getByEmail {}", email);
         return service.getByEmail(email);
     }
+
+    public User getWithMeals(int id) {
+        log.info("getWithMills {}", id);
+        return service.getWithMeals(id);
+    }
 }
